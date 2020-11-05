9ae8ce2bb350afef3b54db3e2dc923661aa39b6e
vihoL <842658064@qq.com>
2020-11-05 10:03:40 +0800

    viho update

--- src/main/java/com/atguigu/jianfa/Jinggangtuifa.java
+++ src/main/java/com/atguigu/jianfa/Jinggangtuifa.java
@@ -4,6 +4,9 @@ public class Jinggangtuifa {
     public static void main(String[] args) {
         System.out.println("铁头功");
         System.out.println("蛤蟆功");
+        System.out.println("蛤蟆功");
+        System.out.println("凌波微步");
+        System.out.println("凌波微步");
         System.out.println("凌波微步");
         System.out.println("凌波微步");
 
