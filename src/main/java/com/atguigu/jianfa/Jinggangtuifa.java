bcd7cfb7a47fdf192c7198dd189db11f1396843a
zhangxinchuin <1383768701@163.com>
2020-11-05 09:48:26 +0800

    添加功法

--- src/main/java/com/atguigu/jianfa/Jinggangtuifa.java
+++ src/main/java/com/atguigu/jianfa/Jinggangtuifa.java
@@ -4,6 +4,8 @@ public class Jinggangtuifa {
     public static void main(String[] args) {
         System.out.println("铁头功");
         System.out.println("蛤蟆功");
+        System.out.println("降龙十八掌");
+        System.out.println("独孤九剑");
     }
 
 }
