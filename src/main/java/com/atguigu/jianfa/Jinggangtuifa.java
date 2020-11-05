6eeb6bd92750ec7f5867f4eb4ddd87fffde45769
vihoL <842658064@qq.com>
2020-11-05 11:04:17 +0800

    tijiao

--- src/main/java/com/atguigu/jianfa/Jinggangtuifa.java
+++ src/main/java/com/atguigu/jianfa/Jinggangtuifa.java
@@ -4,6 +4,7 @@ public class Jinggangtuifa {
     public static void main(String[] args) {
         System.out.println("铁头功");
         System.out.println("蛤蟆功");
+        System.out.println("蛤蟆功");
     }
 
 }
